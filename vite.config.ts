26c364c5bfd96d9e87d1788fe01805f69c5d741c
Ni55aN <huccah@gmail.com>
2023-08-03 22:41:29 +0300

    GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import checker from 'vite-plugin-checker'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
       typescript: true,
       overlay: false
     }),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-9Q2N23SRQJ',
+      },
+    })
   ],
 });
